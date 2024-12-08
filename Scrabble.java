5530105639d32f3b7169278810d03ea96fb5442e
Daniel Levy <daniel.levy04@post.runi.ac.il>
2024-12-08 18:47:54 +0200

    15th commit

--- Scrabble.java
+++ Scrabble.java
@@ -106,7 +106,7 @@ public class Scrabble {
 				System.out.println("Invalid word. Try again.");
 			} else if (isWordInDictionary(input)) {
 				score += wordScore(input);
-				System.out.println(input + "earned " + wordScore(input) + " points. Score: " + score + " points.\n");
+				System.out.println(input + " earned " + wordScore(input) + " points. Score: " + score + " points.\n");
 				hand = MyString.remove(hand, input); 
 				numOfWords ++ ;
 			} else {
